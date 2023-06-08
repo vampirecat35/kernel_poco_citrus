721c434e822281e79e9f1941ca33e9389bb4f3a4
Andy Vandijck <andyvand@gmail.com>
2023-06-08 13:34:12 +0200

    Fixes

--- techpack/camera/drivers/cam_utils/cam_trace.h
+++ techpack/camera/drivers/cam_utils/cam_trace.h
@@ -15,8 +15,8 @@
 
 #include <linux/tracepoint.h>
 #include <media/cam_req_mgr.h>
-#include "cam_req_mgr_core.h"
-#include "cam_req_mgr_interface.h"
+#include "../cam_req_mgr/cam_req_mgr_core.h"
+#include "../cam_req_mgr/cam_req_mgr_interface.h"
 #include "cam_context.h"
 
 #define CAM_DEFAULT_VALUE 0xFF
